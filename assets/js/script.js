fb7d91e22d8b39fe3b9e2ef30988289492a457dc
Thomas Oliver <oliver.tommy@icloud.com>
2024-10-22 13:02:47 +0000

    Add error message if username is not valid

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,23 +3,32 @@ const rulesSection = document.getElementById("rulesSection");
 const submit = document.getElementById("submit");
 const usernameGameDisplay = document.getElementById("usernameGameDisplay");
 const usernameRulesDisplay = document.getElementById("usernameRulesDisplay");
+const usernameError = document.getElementById("username-error");
 
 
 submit.addEventListener("click", getUsername);
 
 function getUsername(event) {
+    // Prevent any default actions occuring when submit button pressed
     event.preventDefault();
+    
+    // Setting username variable to the text input value
+    const username = document.getElementById("username").value;
+
+    // Preventing username from being empty
     if (username === "") {
         console.error("You must enter a username");
+        usernameError.classList.remove("hide");
     } else {
-        username = document.getElementById("username").value;
+        // Display the username in the games and rules sections
+        usernameGameDisplay.textContent = username;
+        usernameRulesDisplay.textContent = `Hi, ${username}`;
+        // Hide the enter name section and show the rules section
+        enterNameSection.classList.add("hide");
+        rulesSection.classList.remove("hide");
     }
-    usernameGameDisplay.textContent = username;
-    usernameRulesDisplay.textContent = `Hi, ${username}`;
-    enterNameSection.classList.add("hide");
-    rulesSection.classList.remove("hide");
 }
 
-function playGame(){
+function playGame() {
 
 }
